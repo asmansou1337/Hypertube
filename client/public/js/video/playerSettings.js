084b2aebf958cd44d1a6d9e69ad45d2844bba2f9
asmansou <asmae.mansouri92@gmail.com>
2020-11-07 13:05:53 +0100

    update

--- client/public/js/video/playerSettings.js
+++ client/public/js/video/playerSettings.js
@@ -7,20 +7,20 @@
  }
  );
  // Styling the video player
- myPlayer.addClass("vjs-styling");
+myPlayer.addClass("vjs-styling");
 
 myPlayer.on('error', function() {
 toastr.error(`${(getCookie('lang') == 'fr') ? 'Le video n\'a pas pu être chargé' : 'The media could not be loaded'}`);
-setTimeout(function(){
- document.querySelector('.videoContainer').style.display = 'none';
-}, 7000)
+// setTimeout(function(){
+//  document.querySelector('.videoContainer').style.display = 'none';
+// }, 7000)
 });
 
 var errorDisplay = myPlayer.getChild('errorDisplay');
 errorDisplay.off(myPlayer, 'error', errorDisplay.open);
    
    function loadTorrent(hash) {
-     document.querySelector('.videoContainer').style.display = 'block';
+    //  document.querySelector('.videoContainer').style.display = 'block';
      var movieId = document.getElementsByClassName('videoContainer') && document.getElementsByClassName('videoContainer')[0].id
      myPlayer.src(
      {
@@ -42,35 +42,35 @@ errorDisplay.off(myPlayer, 'error', errorDisplay.open);
 myPlayer.ready(async function () {
 // Set volume to half by default
 myPlayer.volume(0.5);
-// Get Captions
-var imdb = document.getElementsByClassName('player') && document.getElementsByClassName('player')[0].id      
-const enSubs = await axios.get(`/movies/captions/${imdb}?lang=en`)
-if (enSubs) {
- if (enSubs.data !== "")
-     myPlayer.addRemoteTextTrack(
-       {
-         kind: "captions",
-         src: `/movies/captions/${imdb}?lang=en`,
-         srclang: "en",
-         label: (getCookie('lang') == 'fr') ? "Anglais" : "English",
-       },
-       true
-     );
-}
- 
-const frSubs = await axios.get(`/movies/captions/${imdb}?lang=fr`)
-if (frSubs) {
- if (frSubs.data !== "")
-     myPlayer.addRemoteTextTrack(
-       {
-         kind: "captions",
-         src: `/movies/captions/${imdb}?lang=fr`,
-         srclang: "fr",
-         label: (getCookie('lang') == 'fr') ? "Francais" : "French",
-       },
-       true
-     );
-}
+  // Get Captions
+  var imdb = document.getElementsByClassName('player') && document.getElementsByClassName('player')[0].id      
+  await axios.get(`/movies/captions/${imdb}?lang=en`)
+  .then((resp) => {
+    if (resp.data !== "")
+       myPlayer.addRemoteTextTrack(
+         {
+           kind: "captions",
+           src: `/movies/captions/${imdb}?lang=en`,
+           srclang: "en",
+           label: (getCookie('lang') == 'fr') ? "Anglais" : "English",
+         },
+         true
+       );
+  }).catch()
+   
+  await axios.get(`/movies/captions/${imdb}?lang=fr`)
+  .then((resp) => {
+    if (resp.data !== "")
+       myPlayer.addRemoteTextTrack(
+         {
+           kind: "captions",
+           src: `/movies/captions/${imdb}?lang=fr`,
+           srclang: "fr",
+           label: (getCookie('lang') == 'fr') ? "Francais" : "French",
+         },
+         true
+       );
+  }).catch()
 });
 
 myPlayer.on('play', async function() {
